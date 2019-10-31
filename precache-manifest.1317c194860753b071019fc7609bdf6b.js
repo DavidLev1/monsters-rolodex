31865dddf8e1e4c0eee7d5a8f98c885e6afab0af
David Lev <davidlevman@gmail.com>
2019-10-31 23:18:28 +0200

    Updates

---
+++ precache-manifest.1317c194860753b071019fc7609bdf6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5453c6f4185dc88423f05d94fcf4eef8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "163314cde1a0ee59f582",
+    "url": "/monsters-rolodex/static/css/main.bbcd5d09.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "163314cde1a0ee59f582",
+    "url": "/monsters-rolodex/static/js/main.5b596772.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
