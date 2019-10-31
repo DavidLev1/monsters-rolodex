ba705c3b27d97cdd8e1bd5c9755be4961237d194
David Lev <davidlevman@gmail.com>
2019-10-31 23:20:03 +0200

    Adding files for GitHub pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super(); // Calls to React.Component's constructor (each component is a class)
+
+    this.state = {
+      monsters: [],
+      searchField: '' 
+    }
+
+    //this.handleChange = this.handleChange.bind(this);
+  }
+
+  // When React starting display the component
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        //console.log(response);
+        response.json()
+        
+        .then(users => {
+          //console.log(users);
+          this.setState( {monsters: users})
+        })
+      });
+  }
+
+
+  handleChange = (e) => {
+    this.setState(
+      {searchField: e.target.value}, 
+      //() => console.log(this.state)
+    ); 
+  }
+
+
+  onSearchChange = event => {
+    this.setState({ searchField: event.target.value})
+  }
+
+
+  render() {
+    // const monsters = this.state.monsters;
+    // const searchField = this.state.searchField;
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter( monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+    //console.log(filteredMonsters);
+
+    return (
+      <div className="App">
+
+        <h1> Monsters Rolodex</h1>
+
+        {/* <SearchBox  onSearchChange={this.onSearchChange}/> */}
+
+        <SearchBox 
+          placeholder='search monsters' 
+          handleChange = {this.handleChange} 
+        />
+
+        <CardList monsters={filteredMonsters} /> 
+      </div>
+    );
+  }
 }
+
 
 export default App;
